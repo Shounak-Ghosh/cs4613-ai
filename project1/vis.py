e99df086afe47860ab28d01ed049cc0803d44d12
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2024-10-31 16:48:57 -0400

    update vis py to skip specified number of lines

--- project1/vis.py
+++ project1/vis.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import sys
 
-def plot_maze(file_path):
+def plot_maze(lines_discarded,file_path):
     try:
         # Load the maze from the .txt file
         with open(file_path, "r") as file:
-            maze = np.loadtxt(file_path, delimiter=" ", usecols=range(50))
+            maze = np.loadtxt(file_path, delimiter=" ",skiprows=lines_discarded, usecols=range(50))
 
         # Initialize the plot
         fig, ax = plt.subplots(figsize=(12, 7))
@@ -36,12 +36,14 @@ def plot_maze(file_path):
     except Exception as e:
         print(f"Failed to read the file: {e}")
 
-
 def main():
-    file_path = sys.argv[1]
-    plot_maze(file_path)
-
-
+    if len(sys.argv) != 3:
+        print(f"Usage: python {sys.argv[0]} <num_lines_to_discard> <filename>")
+        sys.exit(1)
+    
+    file_path = sys.argv[2]
+    lines_discarded = int(sys.argv[1])
+    plot_maze(lines_discarded , file_path)
 
 if __name__ == "__main__":
     main()
